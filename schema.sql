6fd3f58afaa6c69692e7d066c4e147f96a3a325e
Alan Johnson <nalanj@fastmail.com>
2020-01-30 17:36:07 -0500

    Get ci run set up

--- schema.sql
+++ schema.sql
@@ -1,24 +1,29 @@
 -- Schema for sqlfun testing
+DROP FUNCTION IF EXISTS "find_show_by_name";
+DROP FUNCTION IF EXISTS "find_show_by_id";
+DROP FUNCTION IF EXISTS "insert_show";
 DROP TABLE shows;
+
 CREATE TABLE shows (
     id integer NOT NULL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
+    name VARCHAR(255) NOT NULL UNIQUE,
+    production_year integer NOT NULL
 );
 
-INSERT INTO shows(id, name) VALUES (
-    1, 'Mr. Wizard'
+INSERT INTO shows(id, name, production_year) VALUES (
+    1, 'Mr. Wizard', 1979
 ),
 (
-    2, 'Gilligan''s Island'
+    2, 'Gilligan''s Island', 1965
 ),
 (
-    3, 'Bewitched'
+    3, 'Bewitched', 1963
 ),
 (
-    4, 'I Love Lucy'
+    4, 'I Love Lucy', 1957
 );
 
-DROP FUNCTION IF EXISTS "find_show_by_name";
+
 CREATE FUNCTION "find_show_by_name"(
   _name text
 ) RETURNS SETOF shows AS $$
@@ -29,7 +34,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-DROP FUNCTION IF EXISTS "find_show_by_id";
 CREATE FUNCTION "find_show_by_id"(
   _id integer
 ) RETURNS SETOF shows AS $$
@@ -41,7 +45,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-DROP FUNCTION IF EXISTS "insert_show";
 CREATE FUNCTION "insert_show"(
   _id integer,
   _name text
